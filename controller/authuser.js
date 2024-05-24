0a20d164eebbead91dc120dd4d0d091287d5c7a7
Muhammad Iqbal <muh.iqbalg1@gmail.com>
2024-05-25 02:20:59 +0700

    feat: add avatar while login

--- controller/authuser.js
+++ controller/authuser.js
@@ -25,34 +25,9 @@ module.exports = {
       return res.status(404).json({ msg: "Password salah" });
     }
     let token = generator.generateAccessToken(
-      { uuid: results.uuid, email: results.email, name: results.name },
+      { uuid: results.uuid, email: results.email, name: results.name, avatar: results.password },
       process.env.SECRET_TOKEN
     );
     return res.json({ token: token, name: results.name });
-  },
-  insert: async (req, res) => {
-    const results = await prisma.user.create({
-      data: {
-        email: req.body.email,
-        name: req.body.name,
-        identityNumber: req.body.identityNumber,
-        password: await genPass.generatePassword(req.body.password),
-        batch: parseInt (req.body.batch),
-        birtday: new Date(req.body.birtday),
-        program_study: req.body.program_study,
-        signature: req.body.signature,
-        phoneNumber: req.body.phoneNumber,
-        telegramId: req.body.telegramId,
-        telegramToken: genPass.generateString(10),
-        nfc_data: req.body.nfc_data,
-      },
-    });
-
-    if (!results) {
-      return res.status(400).json({ msg: "Data yang Diberikan Tidak Valid/Tidak Lengkap" });
-    }
-
-    return res.status(200).json({ msg: "Data yang Diberikan Tidak Valid/Tidak Lengkap" });
-
-  },
+  }
 };
