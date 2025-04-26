b319d6a6e978fe0fbb094933fd9fb1d8ca0cceb8
Mousynano <104660173+Mousynano@users.noreply.github.com>
2025-04-26 13:15:38 +0700

    fix: deleted unused recap.js in routes

--- routes/recap.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const route = express.Router();
-const { handleGetLog, makeRecap } = require('../controller/recap')
-const middleware = require('../middleware')
-route.get("/:uuid", middleware.authorization(), handleGetLog)
-route.post("/", middleware.authorization(), handleGetLog)
-route.get("/download/:uuid", middleware.authorization(), makeRecap)
-// route.post("/download/:uuid", middleware.authorization("get_others_recap"), makeRecap)
-module.exports = route;
